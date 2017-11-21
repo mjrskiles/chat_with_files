88c678baf9f6e58a56dc4e7cb8a2d23046d6994a
Michael Skiles <mjrskiles@gmail.com>
2017-11-20 19:45:28 -0600

    Initial commit.

---
+++ ChatClient.py
@@ -0,0 +1,329 @@
+"""
+CSC376 Assignment 4 - Messenger with files
+
+Michael Skiles
+"""
+
+import threading
+import sys
+import os
+import struct
+import socket
+import getopt
+
+def usage( script_name ):
+    print( 'Argument(s) specified incorrectly.')
+    print( 'Usage: python3 ' + script_name + ' -l <listen port> [-s] [connect server address]\
+                -p <connect server port>' )
+
+argv = sys.argv
+argc = len( sys.argv )
+if argc < 2 or argc > 7 :
+    usage( sys.argv[0] )
+    sys.exit(1)
+
+def parse_opts( argv, argc ):
+    run_as_server = True
+    listen_port = ''
+    host = 'localhost'
+    port = ''
+
+    #options is a list of tuples of the parsed -options and values, args is the leftover
+    options, args = getopt.getopt(argv[1:], "l:s:p:")
+
+    for (opt, val) in options:
+        if opt == '-l':
+            listen_port = val
+            try:
+                int(listen_port)
+            except ValueError:
+                print("The listening port was not specified correctly.")
+                usage( argv[0] )
+
+        if opt == '-s':
+            host = val
+            run_as_server = False
+            if host == '':
+                print("The host was specified incorrectly.")
+                usage( argv[0] )
+
+        if opt == '-p':
+            port = val
+            run_as_server = False
+            try:
+                int(port)
+            except ValueError:
+                print("The server port was not specified correctly.")
+                usage( argv[0] )
+
+#    print(run_as_server)
+#    print(listen_port)
+#    print(host)
+#    print(port)
+
+    if listen_port == '':
+        usage( argv[0] )
+    if not run_as_server:
+        if host == '' or port == '':
+            usage( argv[0] )
+
+    return [run_as_server, listen_port, host, port]
+
+class Messenger:
+
+    def __init__( self, port ):
+        self.port = port
+        self.threads = []
+        self.listen_sock = None
+        self.server_host = None
+        self.server_port = None
+        self.text_sock   = None
+
+    def open_listener( self, port ):
+        """
+        open a socket on the specified port and set it to listen.
+        """
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind( ('localhost', int(port)) )
+            sock.listen(5)
+        except:
+            self.clean_up()
+        return sock
+
+    def request_connection( self, host, port ):
+        """
+        opens a socket and binds it to 'sock'
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect( (host, int(port)) )
+        return sock
+
+    def accept_connection( self ):
+        #socket.accept() returns a (socket, ('host', port)) tuple
+        sock, addr = self.listen_sock.accept()
+        print("Connection accepted, addr: " + str(addr))
+        return (sock, addr)
+
+    def run_messenger( self ):
+        """
+        Start the threads to receive messages and listen for file requests.
+        Then get user input in the main thread.
+        """
+        msg_receiver = threading.Thread( target=self.get_messages )
+        msg_receiver.start()
+        self.threads.append( msg_receiver )
+        file_server = threading.Thread( target=self.serve_files )
+        file_server.start()
+        self.threads.append( file_server )
+        self.get_input()
+
+    def get_messages( self ):
+        """
+        Get messages from the other end
+        """
+        message = self.text_sock.recv( 4096 )
+        while message:
+            print( message.decode(), end='' )
+            message = self.text_sock.recv( 4096 )
+        self.clean_up()
+
+    def get_input( self ):
+        """
+        Get user input from stdin and decide what to do with it.
+        """
+        print("Enter your name:")
+        line = sys.stdin.readline()
+        self.send_text( line )
+        while True:
+            print("Enter an option ('m', 'f', 'x'):")
+            print("  (M)essage (send)")
+            print("  (F)ile (request)")
+            print(" e(X)it")
+            choice = sys.stdin.readline().rstrip('\n')
+            if choice == 'x':
+                break
+            elif choice == 'm':
+                print("Enter your message:")
+                line = sys.stdin.readline()
+                line = 'm:' + line
+                self.send_text( line )
+            elif choice == 'f':
+                print("Who owns the file?")
+                owner = sys.stdin.readline().rstrip('\n')
+                print("Which file do you want?")
+                file_name = sys.stdin.readline().rstrip('\n')
+                request = 'f:' + owner + ':' + file_name
+                self.get_file( request, file_name )
+        self.clean_up()
+
+    def send_text( self, text ):
+        self.text_sock.send( text.encode() )
+
+    def get_file( self, request, file_name ):
+        """
+        Open a new thread to request a file so that we can get back
+        to reading stdin on this thread.
+        """
+#        print(type(file_name))
+#        print(file_name)
+        #There needs to be a comma after file_name because args expects a tuple!!!
+        file_conn = threading.Thread( target=self.request_file, args=(request, file_name) )
+        self.threads.append(file_conn)
+        file_conn.start()
+
+    def request_file( self, request, file_name ):
+        """
+        Gets the client connection info from the server then
+        opens a p2p connection to another client and receives the file"""
+        self.send_text( request )
+        file_req_port = self.text_sock.recv( 4 ).decode()
+        print("Server sent back listen port " + file_req_port + ".")
+        try:
+            int(file_req_port)
+        except ValueError:
+            print("The server did not send back a valid port number.")
+            sys.exit(1)
+        self.server_port = file_req_port
+        # this only works on localhost right now.  Would need to get the server
+        # to send back the client address as well.
+        try:
+            file_sock = self.request_connection( self.server_host, self.server_port )
+            if file_sock:
+                print("file_sock opened")
+            file_sock.send( file_name.encode() )
+            file_size_bytes = file_sock.recv( 4 )
+            if file_size_bytes:
+                file_size= struct.unpack( '!L', file_size_bytes[:4] )[0]
+                if file_size:
+                    Messenger.receive_file( file_sock, file_name )
+                else:
+                    print( 'File does not exist or is empty' )
+            else:
+                print( 'File does not exist or is empty' )
+        except:
+            print("Could not open connection to file server.")
+        finally:
+            if file_sock:
+                file_sock.close()
+
+    def receive_file( sock, filename ):
+        """receive the file lines returned from the server"""
+        file= open( filename, 'wb' )
+        while True:
+            file_bytes= sock.recv( 1024 )
+            if file_bytes:
+                file.write( file_bytes )
+            else:
+                break
+        file.close()
+
+    def serve_files( self ):
+        """
+        Gets connections for file requests.
+        """
+        print("Opening file server...")
+        while True:
+            sock, addr = self.accept_connection()
+            file_server = threading.Thread( target=Messenger.handle_request, args=(sock,) )
+            file_server.start()
+            self.threads.append(file_server)
+
+    def handle_request( sock ):
+        """
+        Get the requested file name and send the file if it exists.
+        """
+        msg_bytes= sock.recv(1024)
+        # convert the message into a string, which is the name of the desired file
+        file_name= msg_bytes.decode()
+        # check whether the file exists; if it does, send back the file size
+        print("Received file name: {}".format(file_name))
+        try:
+            file_stat= os.stat( file_name )
+            if file_stat.st_size:
+                print("Found file {}".format(file_name))
+                file= open( file_name, 'rb' )
+                Messenger.send_file( sock, file_stat.st_size, file )
+            else:
+                print("File not found: {}".format(file_name))
+                Messenger.no_file( sock )
+        except OSError:
+            Messenger.no_file( sock )
+        finally:
+            sock.close()
+
+    def send_file( sock, file_size, file ):
+        """
+        Send the file to the other end.
+        """
+        print( 'File size is ' + str(file_size) )
+        file_size_bytes= struct.pack( '!L', file_size )
+        # send the number of bytes in the file
+        sock.send( file_size_bytes )
+        # read the file and transmit its contents
+        while True:
+            file_bytes= file.read( 1024 )
+            if file_bytes:
+                sock.send( file_bytes )
+            else:
+                break
+        file.close()
+
+    def no_file( sock ):
+        zero_bytes= struct.pack( '!L', 0 )
+        sock.send( zero_bytes )
+
+    def clean_up( self ):
+        self.text_sock.close()
+        self.listen_sock.close()
+        os._exit(0)
+
+
+class Server( Messenger ):
+    """
+    Initialize the server to a standard Messenger state.
+    The purpose of having derived Server and Client classes
+    is basically to initialize 2 Messenger objects to the same
+    starting state.  Once initialized, both server and client
+    are virtually identical in this program.
+    """
+    def __init__( self, listen_port ):
+        super().__init__( listen_port )
+        self.listen_sock = self.open_listener( self.port )
+        self.text_sock, addr = self.accept_connection()
+        self.server_host = addr[0]
+        file_req_port = self.text_sock.recv( 4 ).decode()
+        print("Client sent back listen port " + file_req_port + ".")
+        try:
+            int(file_req_port)
+        except ValueError:
+            print("The client did not send back a valid port number.")
+            sys.exit(1)
+        self.server_port = file_req_port
+
+
+class Client( Messenger ):
+    def __init__( self, listen_port, host, port ):
+        super().__init__( listen_port )
+        self.server_host = host
+        self.server_port = port
+        self.text_sock = self.request_connection( self.server_host, self.server_port )
+        if self.text_sock is None:
+            print("Could not open socket.")
+            sys.exit(1)
+        print("Connection accepted.  Sending listen port " + listen_port + ".")
+        self.text_sock.send( listen_port.encode() )
+        self.listen_sock = self.open_listener( listen_port )
+
+
+def main():
+    parsed_args = parse_opts( argv, len(argv) )
+    if parsed_args[0]:
+        m = Server( parsed_args[1] )
+    else:
+        m = Client( parsed_args[1], parsed_args[2], parsed_args[3] )
+
+    m.run_messenger()
+
+main()
---
+++ ChatServer.py
@@ -0,0 +1,156 @@
+"""
+CSC376 Assignment 3 - Chat
+Server
+Michael Skiles
+"""
+
+import threading
+import sys
+import os
+import socket
+
+def usage( script_name ):
+    print( 'Usage: python3 ' + script_name + ' <port number>' )
+
+argv = sys.argv
+argc = len( sys.argv )
+if argc != 2:
+    usage( sys.argv[0] )
+    os.exit(1)
+
+def parse_opts( argv ):
+    port = ''
+    host = 'localhost'
+
+    try:
+        port = argv[1]
+
+        # make sure the port argument is an int, throws ValueError
+        int(port)
+    except (IndexError, ValueError):
+        print("The port number was either not specified, or not an integer")
+        sys.exit()
+
+    return (port, host)
+
+class Server:
+
+    #The client_socks dict is a dictionary with an client id int
+    #as the key, followed by a list in form [socket, thread instance, client username]
+    MESSAGE_REQUEST = 0
+    FILE_REQUEST = 1
+    BAD_REQUEST = -1
+
+    SOCKET_POS = 0
+    CLIENT_NAME_POS = 1
+    CLIENT_LISTEN_PORT_POS = 2
+
+    client_socks = {}
+    client_names = {}
+    listen_sock = None
+
+    def __init__( self, port ):
+        self.port = port
+        self.open_listener( self.port )
+
+    def open_listener( self, port ):
+        self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_sock.bind( ('localhost', int(port)) )
+        self.listen_sock.listen(5)
+
+    def get_new_client( self ):
+        next_client = 0
+        try:
+            while True:
+                sock, addr = self.listen_sock.accept()
+                print("Connection accepted, addr: " + str(addr) + " client no. " + str(next_client))
+                file_req_port = sock.recv( 4 ).decode()
+                print("Client sent back listen port " + file_req_port + ".")
+                try:
+                    int(file_req_port)
+                except ValueError:
+                    print("The client did not send back a valid port number.")
+                    sock.close()
+                self.client_socks[next_client] = [sock, '', '']
+                self.client_socks[next_client][self.CLIENT_LISTEN_PORT_POS] = file_req_port
+
+                instance = threading.Thread( target=self.get_messages, args=(sock, next_client) )
+#                print("instance created.")
+                instance.start()
+                
+                next_client += 1
+        finally:
+            self.listen_sock.close()
+
+    def get_messages( self, sock, id ):
+        print("Thread opened @client " + str(id))
+        message = sock.recv( 4096 )
+        client_name = message.decode().strip()
+        self.client_socks[id][self.CLIENT_NAME_POS] = client_name
+        self.client_names[client_name] = id
+        print( 'Client@client ' + str(id) + ' entered their name as ' + client_name )
+        request = sock.recv( 4096 )
+        while request:
+            #the client will send a letter indicating the request type, followed by a ':' 
+            #followed by the request text.
+            #example: 'm:this is a text message' or 'f:file_name'
+            request = request.decode()
+            message = request.split(':', 1)
+            code = Server.get_request_type( message[0] )
+            if code == Server.BAD_REQUEST:
+                continue
+            if code == Server.MESSAGE_REQUEST:
+                print( client_name + ': ' + message[1], end='' )
+                self.broadcast_msg( id, message[1] )
+            if code == Server.FILE_REQUEST:
+                message = message[1].split(':', 1)
+                owner = message[0]
+                file_name = message[1]
+                print( client_name + ' requests to get file ' + file_name + ' from ' + owner )
+                self.handle_file_request( sock, owner )
+            request = sock.recv( 4096 )
+        sock.close()
+        self.client_socks.pop(id)
+        print( client_name + ' disconnected and the socket was successfully closed.' )
+        
+    def get_request_type( code ):
+        print(code)
+        if code == 'm':
+            return Server.MESSAGE_REQUEST
+        if code == 'f':
+            return Server.FILE_REQUEST
+        else:
+            return Server.BAD_REQUEST
+
+    def handle_file_request( self, sock, owner ):
+        owner_id = self.client_names[owner]
+        port = self.client_socks[owner_id][self.CLIENT_LISTEN_PORT_POS]
+        try:
+            sock.send( port.encode() )
+        except:
+            print('Error sending back port number for p2p connection.')
+
+    def broadcast_msg( self, client_id, message ):
+        line_to_send = self.client_socks[client_id][self.CLIENT_NAME_POS] + ': ' + message
+        for c in self.client_socks:
+            if c != client_id:
+                sock = self.client_socks[c][self.SOCKET_POS]
+                send_instance = threading.Thread( target=self.send_msg, args=(sock, c, client_id, line_to_send) )
+                send_instance.start()
+
+    def send_msg( self, sock, id_to, id_from, message ):
+        try:
+            sock.send( message.encode() )
+        except:
+            to_name = self.client_socks[id_to][CLIENT_NAME_POS]
+            from_name = self.client_socks[id_from][CLIENT_NAME_POS]
+            print('Sending message to ' + to_name + ' from ' + from_name + ' failed.')
+
+
+def main():
+    port, host = parse_opts(argv)
+    s = Server(port)
+    s.get_new_client()
+
+main()
